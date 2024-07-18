b85986ac376bb37ddabd775e4e9a46776bd6d3ae
Swaiguy <126988847+Swaiguy@users.noreply.github.com>
2024-07-18 09:44:38 +0200

    Update default.nix

--- vars/default.nix
+++ vars/default.nix
@@ -3,5 +3,4 @@
   userfullname = "The Individual Known As";
   useremail = "lastresortcode@gmail.com";
   networking = import ./networking.nix {inherit lib;};
-  users.users.kry.initialHashedPassword = "123";
 }
